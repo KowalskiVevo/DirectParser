a7e4c2ced5aff94274fe813465131569612af719
Koval Dmitriy <D.Koval@hostco.ru>
2023-12-19 22:03:17 +0500

    Убрать аннотацию EnableWebMvc с SwaggerConfiguration. Добавить новую настройку ant_path_matcher для доступа к объектам в папке resources

--- src/main/java/urfu/example/yandexdirect/config/SwaggerConfiguration.java
+++ src/main/java/urfu/example/yandexdirect/config/SwaggerConfiguration.java
@@ -12,7 +12,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableWebMvc
 public class SwaggerConfiguration implements WebMvcConfigurer {
 
   @Bean
