59bac47ef2f66ac5b6ca319cf83d6305432c4f88
Koval Dmitriy <D.Koval@hostco.ru>
2024-01-15 18:51:46 +0500

    add cors

--- src/main/java/urfu/example/yandexdirect/YandexDirectApplication.java
+++ src/main/java/urfu/example/yandexdirect/YandexDirectApplication.java
@@ -3,13 +3,26 @@ package urfu.example.yandexdirect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
 public class YandexDirectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YandexDirectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(YandexDirectApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**");
+      }
+    };
+  }
 
 }
